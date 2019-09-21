b149d9b5f2d0947c4b6665e907e71f1ad4931727
alstjr7375 <alstjr7375@daum.net>
2019-09-21 18:15:26 +0900

    Fix: command not found error

--- zsh-git-completion.plugin.zsh
+++ zsh-git-completion.plugin.zsh
@@ -1,5 +1,5 @@
 # Fork from
 # https://github.com/git/git/blob/master/contrib/completion/git-completion.zsh
 
+zstyle ':completion:*:*:git:*' script "${0:h}/src/git-completion.bash"
 fpath+="${0:h}/src"
-zstyle ':completion:*:*:git:*' script "${0:h}/zsh-git-completion.zsh"
